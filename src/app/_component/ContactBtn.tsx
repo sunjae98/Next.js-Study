55b0cd01273ce4b8030218ea4f586ab6a2c99d4a
kimsunjae <woal9844@gmail.com>
2024-05-18 22:44:23 +0900

    feat : 상담 페이지 네비게이트 적용

--- src/app/_component/ContactBtn.tsx
+++ src/app/_component/ContactBtn.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface NavProps {
   name: string;
   size: string;
@@ -5,10 +7,12 @@ interface NavProps {
 
 export default function ContactBtn({ name, size }: NavProps) {
   return (
-    <div
-      className={`p-4 pl-6 pr-6 h-${size} text-sm text-white font-bold bg-pointColor rounded-lg flex justify-center items-center m-2`}
-    >
-      {name}
-    </div>
+    <Link href="/contact">
+      <div
+        className={`p-4 pl-6 pr-6 h-${size} text-sm text-white font-bold bg-pointColor rounded-lg flex justify-center items-center m-2`}
+      >
+        {name}
+      </div>
+    </Link>
   );
 }
