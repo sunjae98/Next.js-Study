a7bbd51fad7a2180efed259944314691716ad03c
kimsunjae <woal9844@gmail.com>
2024-05-30 15:16:54 +0900

    design : 이미지 개수 및 사이즈 조정

--- src/app/_component/_section/Slide.tsx
+++ src/app/_component/_section/Slide.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { faker } from "@faker-js/faker";
 
 export default function Slide() {
-  const images = Array.from({ length: 24 }, (_, index) => ({
+  const images = Array.from({ length: 36 }, (_, index) => ({
     id: index,
     src: faker.image.urlLoremFlickr(),
     alt: faker.lorem.word(),
@@ -11,26 +11,26 @@ export default function Slide() {
   return (
     <div className="w-full h-screen flex justify-center items-center overflow-x-hidden flex-col">
       <div className="flex w-full animate-loop gap-2 m-1">
-        {images.slice(0, 12).map((image) => (
+        {images.slice(0, 18).map((image) => (
           <div className="flex-shrink-0" key={image.id}>
             <Image
               src={image.src}
               alt={image.alt}
               layout="fixed"
-              width={192}
+              width={190}
               height={80}
             />
           </div>
         ))}
       </div>
       <div className="flex w-full animate-loop-reverse gap-2 m-1">
-        {images.slice(12, 24).map((image) => (
+        {images.slice(18, 36).map((image) => (
           <div className="flex-shrink-0" key={image.id}>
             <Image
               src={image.src}
               alt={image.alt}
               layout="fixed"
-              width={192}
+              width={190}
               height={80}
             />
           </div>
